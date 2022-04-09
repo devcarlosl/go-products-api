3950b4168869c9b4abfd99c39542b14f9b1ebd1a
Carlos Lopes <dev.carloslopes@gmail.com>
2022-04-09 18:06:06 -0300

    add product handlers

---
+++ pkg/products/get_products.go
@@ -0,0 +1,16 @@
+package products
+
+import (
+	"github.com/devcarlosl/go-products-api/pkg/common/models"
+	"github.com/gofiber/fiber/v2"
+)
+
+func (h handler) GetProducts(c *fiber.Ctx) error {
+	var products []models.Product
+
+	if result := h.DB.Find(&products); result.Error != nil {
+		return fiber.NewError(fiber.StatusNotFound, result.Error.Error())
+	}
+
+	return c.Status(fiber.StatusOK).JSON(&products)
+}
